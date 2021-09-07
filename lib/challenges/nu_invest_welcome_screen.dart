98209832781e6eac32db75af0afef48f4de56555
Mauricio Barbosa <mauriiciobarbosa@gmail.com>
2021-09-07 17:55:49 -0300

    add animation to nu invest welcome screen.

--- lib/challenges/nu_invest_welcome_screen.dart
+++ lib/challenges/nu_invest_welcome_screen.dart
@@ -1,7 +1,48 @@
 import 'package:animation_sample/components/nu_invest_button.dart';
 import 'package:flutter/material.dart';
 
-class NuInvestWelcomeScreen extends StatelessWidget {
+/*
+ * First try
+ */
+class NuInvestWelcomeScreen extends StatefulWidget {
+  @override
+  _NuInvestWelcomeScreenState createState() => _NuInvestWelcomeScreenState();
+}
+
+class _NuInvestWelcomeScreenState extends State<NuInvestWelcomeScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: Duration(seconds: 2),
+    vsync: this,
+  )..forward();
+  late final Animation<Offset> _animationLogo = Tween<Offset>(
+    begin: Offset(0, -1.5),
+    end: Offset.zero,
+  ).animate(CurvedAnimation(
+    parent: _controller,
+    curve: Curves.easeInQuart,
+  ));
+  late final Animation<Offset> _animationWords = Tween<Offset>(
+    begin: Offset(-1.5, 0),
+    end: Offset.zero,
+  ).animate(CurvedAnimation(
+    parent: _controller,
+    curve: Curves.easeInQuart,
+  ));
+  late final Animation<Offset> _animationButtons = Tween<Offset>(
+    begin: Offset(0, 1.8),
+    end: Offset.zero,
+  ).animate(CurvedAnimation(
+    parent: _controller,
+    curve: Curves.easeInQuart,
+  ));
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,21 +58,27 @@ class NuInvestWelcomeScreen extends StatelessWidget {
             top: 32,
             left: 16,
             right: 16,
-            child: Image.asset(
-              'assets/images/nu_invest_logo.png',
-              alignment: Alignment.bottomLeft,
+            child: SlideTransition(
+              position: _animationLogo,
+              child: Image.asset(
+                'assets/images/nu_invest_logo.png',
+                alignment: Alignment.bottomLeft,
+              ),
             ),
           ),
           Positioned(
             bottom: 160,
             left: 16,
             right: 16,
-            child: Text(
-              'Easynvest agora é Nu invest',
-              style: TextStyle(
-                fontSize: 32,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
+            child: SlideTransition(
+              position: _animationWords,
+              child: Text(
+                'Easynvest agora é Nu invest',
+                style: TextStyle(
+                  fontSize: 32,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
           ),
@@ -39,11 +86,14 @@ class NuInvestWelcomeScreen extends StatelessWidget {
             bottom: 120,
             left: 16,
             right: 16,
-            child: Text(
-              'Faça parte da revolução roxa',
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.white,
+            child: SlideTransition(
+              position: _animationWords,
+              child: Text(
+                'Faça parte da revolução roxa',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
               ),
             ),
           ),
@@ -51,20 +101,23 @@ class NuInvestWelcomeScreen extends StatelessWidget {
             bottom: 32,
             left: 16,
             right: 16,
-            child: Row(
-              children: [
-                NuInvestButton(
-                  text: 'Abrir conta',
-                  style: NuInvestButtonStyle.SECONDARY,
-                  onPressed: () => Navigator.pop(context),
-                ),
-                SizedBox(width: 16),
-                NuInvestButton(
-                  text: 'Entrar',
-                  style: NuInvestButtonStyle.PRIMARY,
-                  onPressed: () => Navigator.pop(context),
-                ),
-              ],
+            child: SlideTransition(
+              position: _animationButtons,
+              child: Row(
+                children: [
+                  NuInvestButton(
+                    text: 'Abrir conta',
+                    style: NuInvestButtonStyle.SECONDARY,
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                  SizedBox(width: 16),
+                  NuInvestButton(
+                    text: 'Entrar',
+                    style: NuInvestButtonStyle.PRIMARY,
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                ],
+              ),
             ),
           )
         ],
